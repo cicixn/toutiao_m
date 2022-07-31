2d3a8f301755d24b3965eaf72825acd762861a22
cici <cici_xn@163.com>
2022-07-31 11:18:52 +0800

    my页面完成

--- src/views/My/index.vue
+++ src/views/My/index.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>我的</div>
+  <div>
+    <!-- 头部 -->
+    <header>
+      <div class="login" v-if="isLogin">已登录</div>
+      <div class="logout" v-else>未登录</div>
+    </header>
+    <!-- 中间 -->
+    <main>收藏/历史/小智</main>
+    <!-- 底部 -->
+    <footer>
+      <button v-if="isLogin" @click="logout">退出</button>
+    </footer>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    isLogin() {
+      return !!this.$store.state.tokenObj.token
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.commit('SET_TOKEN', {})
+    }
+  }
+}
 </script>
 
 <style></style>
