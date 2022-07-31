7c642d6eb8f1ae4857b36a67aad2175603d253b0
cici <cici_xn@163.com>
2022-07-31 11:45:32 +0800

    my页面完成header

--- src/views/My/index.vue
+++ src/views/My/index.vue
@@ -3,7 +3,10 @@
     <!-- 头部 -->
     <header>
       <div class="login" v-if="isLogin">已登录</div>
-      <div class="logout" v-else>未登录</div>
+      <div class="logout" v-else>
+        未登录
+        <img src="../../assets/images/登录手机图.png" alt="" />
+      </div>
     </header>
     <!-- 中间 -->
     <main>收藏/历史/小智</main>
@@ -29,4 +32,23 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped lang="less">
+header {
+  width: 100%;
+  height: 400px;
+  background: url(http://110.42.202.109/toutiao/img/banner.d4257372.png)
+    no-repeat 50% / cover;
+  .logout {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    img {
+      width: 132px;
+      height: 132px;
+    }
+  }
+}
+</style>
